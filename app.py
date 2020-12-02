2224529ca76ce09a11a27fae6bb5931e09bc0cd9
Dan-O3O <57209058+Dan-O3O@users.noreply.github.com>
2020-12-01 22:00:08 -0500

    Few more updates on account management

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import os
 from flask import Flask, render_template, request, redirect, flash
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
+from wtforms import ValidationError
 from forms import *
 from flask_bootstrap import Bootstrap
-from flask_login import UserMixin
+from flask_login import UserMixin, LoginManager, login_required, login_user, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
@@ -16,18 +17,36 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'databases.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diet.db'
 
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
 
 db = SQLAlchemy(app)
 manager = Manager(app)
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
 	#__tablename__ = 'user'
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(50), unique=True, index=True, nullable=False)
-	password = db.Column(db.String(80), nullable=False)
+	password_hash = db.Column(db.String(128))
 
-	# diet_plan = db.relationship('DietPlan', backref='user')
+	@property
+	def password(self):
+		raise AttributeError('password is not a readable attribute')
+
+	@password.setter
+	def password(self, password):
+		self.password_hash = generate_password_hash(password)
+
+	def verify_password(self, password):
+		return check_password_hash(self.password_hash, password)
+
+	#diet_plan = db.relationship('DietPlan', backref='user')
 
 
 class DietPlan(db.Model):
@@ -39,82 +58,91 @@ class DietPlan(db.Model):
 	evening_meal = db.Column(db.String(50), nullable=False)
 	claories_in = db.Column(db.String(50))
 
-	# user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-	# user = db.relationship('User', backref='diet')
+	#user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+	#user = db.relationship('User', backref='diet')
 
-# class LogData(db.Model):
-# 	id = db.Column(db.Integer, primary_key=True)
-# 	username = db.Column(db.String(50), unique=True, index=True, nullable=False)
-# 	password = db.Column(db.String(80), nullable=False)
+class Info(db.Model):
+	__tablename__ = 'info'
+	id = db.Column(db.Integer, primary_key=True)
+	first_name = db.Column(db.String(50), nullable=False)
+	last_name = db.Column(db.String(50), nullable=False)
+	weight = db.Column(db.String(50), nullable=False)
+	height = db.Column(db.String(50), nullable=False)
+	expected_calories = db.Column(db.String(50), nullable=False)
 
 @app.route('/', methods=['POST', 'GET'])
+@login_required
 def index():
-	if request.method == "POST" and request.form.get("add"):
-		day = request.form['day']
-		morning = request.form['morning']
-		noon = request.form['noon']
-		evening = request.form['evening']
-		calories = request.form['calories']
-		day_plan = DietPlan(day=day, morning_meal=morning, noon_meal=noon, 
-			evening_meal=evening, claories_in=calories)
-		try:
-			db.session.add(day_plan)
+	status = current_user.is_active
+	if status:
+		if request.method == "POST" and request.form.get("add"):
+			day = request.form['day']
+			morning = request.form['morning']
+			noon = request.form['noon']
+			evening = request.form['evening']
+			calories = request.form['calories']
+			day_plan = DietPlan(day=day, morning_meal=morning, noon_meal=noon, 
+				evening_meal=evening, claories_in=calories)
+			try:
+				db.session.add(day_plan)
+				db.session.commit()
+				return redirect('/')
+			except:
+				return "There was an error adding the plan!"
+
+		elif request.method == "POST" and request.form.get("add_info"):
+			f_name = request.form['f_name']
+			l_name = request.form['l_name']
+			weight = request.form['weight']
+			height = request.form['height']
+			exp_calories = request.form['calories']
+			new_info = Info(first_name=f_name, last_name=l_name, weight=weight, 
+				height=height, expected_calories=exp_calories)
+			try:
+				db.session.add(new_info)
+				db.session.commit()
+				return redirect('/')
+			except:
+				return "There was an error adding the user!"
+
+		elif request.method == 'POST' and request.form.get("morning_update"):
+			new_mor = request.form.get("new_mor")
+			old_mor = request.form.get("old_mor")
+			plan = DietPlan.query.filter_by(morning_meal=old_mor).first()
+			plan.morning_meal = new_mor
 			db.session.commit()
 			return redirect('/')
-		except:
-			return "There was an error adding the plan!"
 
-	# elif request.method == "POST" and request.form.get("add_user"):
-	# 	f_name = request.form['f_name']
-	# 	l_name = request.form['l_name']
-	# 	weight = request.form['weight']
-	# 	height = request.form['height']
-	# 	exp_calories = request.form['calories']
-	# 	new_user = User(first_name=f_name, last_name=l_name, weight=weight, 
-	# 		height=height, expected_calories=exp_calories)
-	# 	try:
-	# 		db.session.add(new_user)
-	# 		db.session.commit()
-	# 		return redirect('/')
-	# 	except:
-	# 		return "There was an error adding the user!"
+		elif request.method == 'POST' and request.form.get("noon_update"):
+			new_noon = request.form.get("new_noon")
+			old_noon = request.form.get("old_noon")
+			plan = DietPlan.query.filter_by(noon_meal=old_noon).first()
+			plan.noon_meal = new_noon
+			db.session.commit()
+			return redirect('/')
 
-	elif request.method == 'POST' and request.form.get("morning_update"):
-		new_mor = request.form.get("new_mor")
-		old_mor = request.form.get("old_mor")
-		plan = DietPlan.query.filter_by(morning_meal=old_mor).first()
-		plan.morning_meal = new_mor
-		db.session.commit()
-		return redirect('/')
+		elif request.method == 'POST' and request.form.get("evening_update"):
+			new_eve = request.form.get("new_eve")
+			old_eve = request.form.get("old_eve")
+			plan = DietPlan.query.filter_by(evening_meal=old_eve).first()
+			plan.evening_meal = new_eve
+			db.session.commit()
+			return redirect('/')
 
-	elif request.method == 'POST' and request.form.get("noon_update"):
-		new_noon = request.form.get("new_noon")
-		old_noon = request.form.get("old_noon")
-		plan = DietPlan.query.filter_by(noon_meal=old_noon).first()
-		plan.noon_meal = new_noon
-		db.session.commit()
-		return redirect('/')
+		elif request.method == 'POST' and request.form.get("calories_update"):
+			new_cal = request.form.get("new_cal")
+			old_cal = request.form.get("old_cal")
+			plan = DietPlan.query.filter_by(claories_in=old_cal).first()
+			plan.claories_in = new_cal
+			db.session.commit()
+			return redirect('/')
 
-	elif request.method == 'POST' and request.form.get("evening_update"):
-		new_eve = request.form.get("new_eve")
-		old_eve = request.form.get("old_eve")
-		plan = DietPlan.query.filter_by(evening_meal=old_eve).first()
-		plan.evening_meal = new_eve
-		db.session.commit()
-		return redirect('/')
-
-	elif request.method == 'POST' and request.form.get("calories_update"):
-		new_cal = request.form.get("new_cal")
-		old_cal = request.form.get("old_cal")
-		plan = DietPlan.query.filter_by(claories_in=old_cal).first()
-		plan.claories_in = new_cal
-		db.session.commit()
-		return redirect('/')
-
+		else:
+			diets = DietPlan.query.all()
+			infos = Info.query.all()
+			return render_template('main.html', diets=diets, infos=infos)
 	else:
-		diets = DietPlan.query.all()
-		# users = User.query.all()
-		return render_template('main.html', diets=diets)
+		return redirect('/login')
 
 
 @app.route('/update/<int:id>', methods=['POST', 'GET'])
@@ -145,27 +173,31 @@ def delete(id):
     except:
         return "There was an error deleting the plan!"
 
-# @app.route('/delete_user/<int:id>')
-# def delete_user(id):
-#     plan_to_delete = User.query.get_or_404(id)
-#     try:
-#         db.session.delete(plan_to_delete)
-#         db.session.commit()
-#         return redirect('/')
-#     except:
-#         return "There was an error deleting the user!"
+@app.route('/delete_info/<int:id>')
+def delete_info(id):
+    plan_to_delete = Info.query.get_or_404(id)
+    try:
+        db.session.delete(plan_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "There was an error deleting the info!"
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
 	form = SignupForm()
 	if form.validate_on_submit():
 		new_user = User(username=form.username.data, password=form.password.data)
-		try:
-			db.session.add(new_user)
-			db.commit()
-			return redirect('/login')
-		except:
-			return "There was an error registrating the user!"
+		if User.query.filter_by(username=form.username.data).first():
+			flash('Username already in use, try another one.')
+		else:
+			try:
+				db.session.add(new_user)
+				db.session.commit()
+				return redirect('/login')
+			except:
+				return "There was an error registrating the user!"
+
 	return render_template('signup.html', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -173,9 +205,28 @@ def login():
 	form = LoginForm()
 	if form.validate_on_submit():
 		user = User.query.filter_by(username=form.username.data).first()
-		if user is not None and user.verify_password(form.passowrd.data):
+		if user is not None and user.verify_password(form.password.data):
+			login_user(user)
 			return redirect('/')
+		flash('Wrong username or password')
 	return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	flash('You have been logged out.')
+	return redirect('/login')
+
+@app.route('/changepassword/<int:id>', methods=['POST', 'GET'])
+def changepassword(id):
+    user_to_update = User.query.get_or_404(id)
+    if request.method == "POST":
+    	user_to_update.password = request.form['password']
+    	db.session.commit()
+    	flash('Please log in again since you have changed your password.')
+    	return redirect('/logout')
+    return render_template('changepassword.html', user_to_update=user_to_update)
 
 if __name__ == '__main__':
 	manager.run()
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, ValidationError
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
@@ -12,10 +12,6 @@ class SignupForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	submit = SubmitField('Sign up')
-
-	# def validate_username(self, field):
-	# 	if app.LogData.query.filter_by(username=field.data).first():
-	# 		raise ValidationError('Username already in use.')
 
 
 
